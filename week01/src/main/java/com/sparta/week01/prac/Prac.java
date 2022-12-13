9d75468c6b68f77e2b7c7f38a7d56faea7728457
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2022-12-13 23:41:46 +0900

    221213

--- week01/src/main/java/com/sparta/week01/prac/Prac.java
+++ week01/src/main/java/com/sparta/week01/prac/Prac.java
@@ -2,6 +2,13 @@ package com.sparta.week01.prac;
 
 public class Prac {
     public static void main(String[] args) {
+        int myAge = 20;
+        boolean isAdult = myAge > 20;
+        boolean isKid = myAge <= 20;
+        System.out.println(isAdult);
+        System.out.println(isKid);
+        System.out.println( myAge == 20); //true
+        System.out.println( myAge != 20); //false
 
     }
 }
